8dbfa133819238450459f85069dc31c6dfc2f445
levg34 <guilhem.dross@laposte.net>
2018-01-12 14:49:41 +0100

    working with file detection

---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require('express')
+var request = require('request')
+const fs = require('fs')
+const app = express()
+const baseURL = 'http://localhost:3000'
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+	res.sendFile(__dirname + '/view/index.html')
+})
+
+function parseLog(body) {
+	var res = []
+	var bodyLines = body.split(/[\n\r]/)
+	var log = ''
+	bodyLines.forEach(line => {
+		const dateLength = 10
+		if (line.length >= dateLength && new Date(line.substring(0, dateLength))!='Invalid Date') {
+			if (log) res.push({log:log,date:log.split('ERROR')[0].trim()})
+			log = ''
+		}
+		log += line + '\n'
+	})
+	return res
+}
+
+function fileExists(file) {
+	return true
+}
+
+app.get('/logs', function (req, res) {
+	var logFile = 'application.log'
+	if (req.query.file&&fileExists(req.query.file)) {
+		logFile = req.query.file
+	}
+	const url = baseURL+'/data/' + logFile
+	var resJSON = {}
+
+	request(url, function (error, response, body) {
+		if (error) {
+			resJSON.error = error
+		} else if (response.statusCode===200) {
+			var parsedList = parseLog(body)
+			resJSON.list = parsedList
+		} else {
+			resJSON.response = response
+		}
+		res.json(resJSON)
+	})
+})
+
+app.get('/logfiles', function (req, res) {
+	const logFolder = './public/data'
+	var files = []
+
+	fs.readdir(logFolder, (err, files) => {
+		files.forEach(file => {
+			files.push(file)
+		})
+	})
+	res.json({files:files})
+})
+
+app.listen(3000, function () {
+	console.log('Example app listening on port 3000!')
+})
